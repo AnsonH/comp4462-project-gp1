05462326474c4de9350bcc0631f3f4d023455acd
owen-hwlee <hwolee@connect.ust.hk>
2021-10-27 19:55:26 +0800

    Added d3.js and useStates filters

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,10 @@
-// import
+import { useState } from "react";
 
 export default function Index(props) {
+  const [Region, setRegion] = useState(""); // Set region to whole world / Asia / other regions
+  const [Year, setYear] = useState(null);
+  const [TopK, setTopK] = useState(6); // Top K factors to display in chord diagram
+
   return (
     <>
       <h1>COMP4462 Project Group 1</h1>
